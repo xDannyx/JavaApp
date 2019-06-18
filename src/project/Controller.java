a1a5d23e4a4d122b8fd05abba9c2b5431d5f30b2
xDannyx <danix7123@gmail.com>
2019-06-18 14:25:23 +0200

    dodany main panel

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
