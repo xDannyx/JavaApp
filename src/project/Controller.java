89350dc04182efb63ef866df620b63d8b4571b84
xDannyx <danix7123@gmail.com>
2019-06-22 17:09:17 +0200

    dodany panel edycji, kilka zmian z przejściami

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -39,6 +39,7 @@ public class Controller implements Initializable {
         y = event.getSceneY();
     }
 
+    //funkcja do zamkniecia aplikacji
     public void closeWindow(javafx.event.ActionEvent actionEvent) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
@@ -49,6 +50,7 @@ public class Controller implements Initializable {
 
     }
 
+    //zmiana na panel rejestracji
     @FXML
     public void switchToRegisterPanel(ActionEvent actionEvent) throws IOException {
         Parent blah = FXMLLoader.load(getClass().getResource("registerPanel.fxml"));
@@ -59,9 +61,45 @@ public class Controller implements Initializable {
         appStage.show();
     }
 
+
+    //zmiana na panel logowania
     @FXML
     public void switchToLoginPanel(ActionEvent actionEvent) throws IOException {
         Parent blah = FXMLLoader.load(getClass().getResource("loginPanel.fxml"));
+        Scene scene = new Scene(blah);
+        scene.setFill(Color.TRANSPARENT);
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    //zmiana na glowny panel
+    @FXML
+    public void switchToEditPanel(ActionEvent actionEvent) throws IOException {
+        Parent blah = FXMLLoader.load(getClass().getResource("editPanel.fxml"));
+        Scene scene = new Scene(blah);
+        scene.setFill(Color.TRANSPARENT);
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    //zmiana na panel edycji
+    @FXML
+    public void switchToMainPanel(ActionEvent actionEvent) throws IOException {
+        Parent blah = FXMLLoader.load(getClass().getResource("mainPanel.fxml"));
+        Scene scene = new Scene(blah);
+        scene.setFill(Color.TRANSPARENT);
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    //przejscie do glownego poanelu PO zalogowaniu
+    /** TO JEST DO EDYCJI, PÓKI CO TYLKO TYMCZASOWO ŻEBY ZMIENIĆ PANEL **/
+    @FXML
+    public void afterLogin(ActionEvent actionEvent) throws IOException {
+        Parent blah = FXMLLoader.load(getClass().getResource("mainPanel.fxml"));
         Scene scene = new Scene(blah);
         scene.setFill(Color.TRANSPARENT);
         Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
